6a5e03aa9d082e7304fbde7d425ad302ff16ba8d
PJMantoss <joelptoss@gmail.com>
2022-01-16 20:56:03 +0100

    update server codes

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 
+mongoose.connect();
+
 app.listen(3001, () => {
     console.log("Server is runnning!")
 });
