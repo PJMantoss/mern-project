d0578790a2eea45edcbfdfc01e3c081fca6b158a
PJMantoss <joelptoss@gmail.com>
2022-01-17 22:13:19 +0100

    update server

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 
-mongoose.connect();
+mongoose.connect("mongodb+srv://joel:hollywood@cluster0.kxgzl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
 
 app.listen(3001, () => {
     console.log("Server is runnning!")
