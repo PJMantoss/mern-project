e86aa44e1c847368297fb1f9f8cabd98754fa5a2
PJMantoss <joelptoss@gmail.com>
2022-01-18 16:41:33 +0100

    update server

--- server/index.js
+++ server/index.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const UserSchema = require("/models/Users");
 
 mongoose.connect("mongodb+srv://joel:hollywood@cluster0.kxgzl.mongodb.net/mernproject?retryWrites=true&w=majority");
+
+app.get("/getusers/", (req, res) => {
+    UserSchema.find()
+});
 
 app.listen(3001, () => {
     console.log("Server is runnning!")
