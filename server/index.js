22ea60eb7fa481e1f2a31ee35157b703742dc992
PJMantoss <joelptoss@gmail.com>
2022-01-18 16:50:10 +0100

    update server

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const UserSchema = require("/models/Users");
 
 mongoose.connect("mongodb+srv://joel:hollywood@cluster0.kxgzl.mongodb.net/mernproject?retryWrites=true&w=majority");
 
-app.get("/getusers/", (req, res) => {
+app.get("/getusers", (req, res) => {
     UserSchema.find({}, (err, result) => {
         if(err){
             res.json(err);
